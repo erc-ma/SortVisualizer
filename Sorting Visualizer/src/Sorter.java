1d04e65e632333498a2153d63f3c3ed74b546671
Eric <103232379+erc-ma@users.noreply.github.com>
2022-09-01 22:18:18 -0400

    Update Sorter.java

--- Sorting Visualizer/src/Sorter.java
+++ Sorting Visualizer/src/Sorter.java
@@ -1,193 +1,104 @@
-0import javax.swing.*;
 import java.awt.Color;
-import java.awt.Graphics;
-import java.util.Scanner;//
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.*;
 
-public class Sorter{
+import javax.imageio.ImageIO;
+
+public class Driver {
+
+
 	public static void main(String[] args) {
 
-		int goAgain;
+        int goAgain;
 		do {
-			JFrame JFrame1 = new JFrame();
-			Scanner reader = new Scanner(System.in);
+            int volume = 0;
 
-			JFrame1.setTitle("My Rectangles");
-			JFrame1.setResizable(false);
-			JFrame1.setSize(1210, 800);
-			JFrame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			CustomPanel cp1;
-
-			String numRects = JOptionPane.showInputDialog("How many rectangles should I draw?");
-			int numR = Integer.parseInt(numRects);
-
-			String sortType = JOptionPane.showInputDialog("Bubble(1)\nSelection(2)\nInsertion(3)");
-			int TSort = Integer.parseInt(sortType);
-
-			Rectangle[] rects = new Rectangle[numR];
-			for (int i = 0; i < rects.length; i++) {
-				rects[i] = new Rectangle();// Make random
-				rects[i].setHeight((int) (Math.random() * 765));
+		//ADD USER INPUT set myDirectoryPath
+        String path = JOptionPane.showInputDialog("What is the pathname for your directory?");
+        String thicknessString = JOptionPane.showInputDialog("How thick are your cross-sections (micrometers)");
+        int thickness = Integer.parseInt(thicknessString);
+		File dir = new File(path);
+		File[] directoryListing = dir.listFiles();
+		if (directoryListing != null) {
+			for (File child : directoryListing) {
+				volume+=executeProgram(child)*thickness;
 			}
+		} 
+        JOptionPane.showMessageDialog(null,"Your volume is "+volume+" px by um");
 
-			cp1 = new CustomPanel(rects, 100, numR);
-			if (cp1.getRects().length != 0)
-				Rectangle.setWidth((int) (1200 / cp1.getRects().length));// -1
-
-			JFrame1.getContentPane().add(cp1);
-			JFrame1.setVisible(true);
-			JFrame1.repaint();
-			if (TSort == 1) {// BUBBLE
-				bubbleSort(cp1, JFrame1);
-			} else if (TSort == 2) {// SELECTION
-				selectionSort(cp1, JFrame1);
-			} else {// INSERTION
-				insertionSort(cp1, JFrame1);
-			}
-
-			JOptionPane.showMessageDialog(null, "Done Drawing");
 			goAgain = JOptionPane.showConfirmDialog(null, "Do you want to go again?");
-			JFrame1.setVisible(false);
 		} while (goAgain == JOptionPane.YES_OPTION);
-		// JFrame1.setVisible(false);
-		// JFrame1.repaint();
+
+	
 
 	}
 
-	public static void bubbleSort(CustomPanel cp1, JFrame JFrame1) {
-		Rectangle[] rects = cp1.getRects();
-		boolean YSwitch = true;
-		while (YSwitch == true) {
-			boolean a = false;
-			for (int i = 0; i < rects.length - 1; i++) {
+	public static int executeProgram(File originalImage) {
 
-				if (rects[i].compareTo(rects[i + 1]) > 0) {
-					for (int j = 1; j < 5; j++) {
-						cp1.getRects()[i].setColor(j % 2);
-						cp1.getRects()[i + 1].setColor(j % 2);
-						JFrame1.repaint();
-						try {
-							Thread.sleep(5);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-					Rectangle temp = rects[i];
-					rects[i] = rects[i + 1];
-					rects[i + 1] = temp;
-					a = true;
+		SelectedRegion reg = new SelectedRegion();
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(originalImage);
+
+			scanImage(img, reg); // IF BUTTON IS PRESSED, scan image for PT
+            return reg.getArea();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+            return 0;
+		}
+	}
+
+	public static void scanImage(BufferedImage img, SelectedRegion reg) {
+		for (int x = 0; x < img.getWidth(); x++) { // goes through all pixels of the image and
+			for (int y = 0; y < img.getHeight(); y++) {
+				if (hueDistance(Constants.COLOR_PT, new Color(img.getRGB(x, y))) <= hueDistance(Constants.COLOR_BG,
+						new Color(img.getRGB(x, y)))
+						&& hueDistance(Constants.COLOR_PT, new Color(img.getRGB(x, y))) <= hueDistance(Constants.COLOR_TH,
+								new Color(img.getRGB(x, y)))) { // if PT+
+					int[] temp = { x, y }; // insert the coordinates of the pixel
+					reg.update(temp);
 				}
+
 			}
-			if (a == false)
-				YSwitch = false;
-			JFrame1.repaint();
 		}
 
 	}
 
-	public static void selectionSort(CustomPanel cp1, JFrame JFrame1) {
-		Rectangle[] rects = cp1.getRects();
-		for (int i = 0; i < rects.length - 1; i++) {
-			int minIndex = i;
-			for (int j = i; j < rects.length; j++)
-				if (rects[j].compareTo(rects[minIndex]) < 0)
-					minIndex = j;
-			for (int j = 1; j < 5; j++) {
-				cp1.getRects()[i].setColor(j % 2);
-				cp1.getRects()[minIndex].setColor(j % 2);
-				JFrame1.repaint();
-				try {
-					Thread.sleep(5);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			Rectangle temp = rects[minIndex];
-			rects[minIndex] = rects[i];
-			rects[i] = temp;
 
-			JFrame1.repaint();
-			try {
-				Thread.sleep(5);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+	public static int getHue(Color a) {
 
-	public static void insertionSort(CustomPanel cp1, JFrame JFrame1) {
-		Rectangle[] rects = cp1.getRects();
-		for (int i = 1; i < rects.length; i++) {
-			Rectangle value = rects[i];
-			int spot = i - 1;
-			while (spot > -1 && value.compareTo(rects[spot]) < 0) {
-				for (int j = 1; j < 5; j++) {
-					cp1.getRects()[spot].setColor(j % 2);// flash the one moving
-					JFrame1.repaint();
-					try {
-						Thread.sleep(5);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-				rects[spot + 1] = new Rectangle(rects[spot]);
+		float min = Math.min(Math.min(a.getRed(), a.getGreen()), a.getBlue());
+		float max = Math.max(Math.max(a.getRed(), a.getGreen()), a.getBlue());
 
-				spot--;
-				JFrame1.repaint();
-				try {
-					Thread.sleep(5);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			
-			for (int j = 1; j < 5; j++) {
-				cp1.getRects()[spot+1].setColor(j % 2);// flash the one moving
-				JFrame1.repaint();
-				try {
-					Thread.sleep(5);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			rects[spot + 1] = value;
-			try {
-				Thread.sleep(5);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-
+		if (min == max) {
+			return 0;
 		}
 
+		float hue = 0f;
+		if (max == a.getRed()) {
+			hue = (a.getGreen() - a.getBlue()) / (max - min);
+
+		} else if (max == a.getGreen()) {
+			hue = 2f + (a.getBlue() - a.getRed()) / (max - min);
+
+		} else {
+			hue = 4f + (a.getRed() - a.getGreen()) / (max - min);
+		}
+
+		hue = hue * 60;
+		if (hue < 0)
+			hue = hue + 360;
+		return Math.round(hue);
 	}
-	// public static void insertionSort1(CustomPanel cp1, JFrame JFrame1) {
-	// Rectangle[] rects = cp1.getRects();
-	// for (int i = 1; i < rects.length; i++) {
-	// Rectangle value = rects[i];
-	// int spot = i - 1;
-	//
-	// while (spot > -1 && value.compareTo(rects[spot]) < 0) {
-	// rects[spot + 1] = rects[spot];
-	// spot--;
-	// rects[spot + 2] = new Rectangle(rects[spot + 2]);
-	// for (int j = 1; j < 5; j++) {
-	// cp1.getRects()[spot + 1].setColor(j % 2);// flash the one moving
-	// JFrame1.repaint();
-	// try {
-	// Thread.sleep(200);
-	// } catch (InterruptedException e) {
-	// e.printStackTrace();
-	// }
-	// }
-	//
-	// }
-	// rects[spot + 1] = value;
-	// JFrame1.repaint();
-	// try {
-	// Thread.sleep(200);
-	// } catch (InterruptedException e) {
-	// e.printStackTrace();
-	// }
-	// }
-	// }
+
+	public static int hueDistance(Color a, Color b) { // returns the distance of 2 colors based on hue
+		float avgHue = (getHue(a) + getHue(b)) / 2;
+//		System.out.print(Math.round(Math.abs(getHue(a) - avgHue)));
+		return Math.round(Math.abs(getHue(a) - avgHue));
+	}
 
 }
